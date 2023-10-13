75dd126335b0423d8764a60363e7113d692e136e
Giovanni Lopes <giovanniclopes@gmail.com>
2023-10-13 16:14:44 -0300

    user validation created

--- src/main/java/br/com/giovannilopes/todolist/user/IUserRepository.java
+++ src/main/java/br/com/giovannilopes/todolist/user/IUserRepository.java
@@ -1,5 +1,9 @@
 package br.com.giovannilopes.todolist.user;
 
-public interface IUserRepository {
-  
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository  extends JpaRepository<UserModel, UUID>{
+  UserModel findByUsername(String username);
 }
--- src/main/java/br/com/giovannilopes/todolist/user/UserController.java
+++ src/main/java/br/com/giovannilopes/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.giovannilopes.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
-  /*
-   * String (texto)
-   * Integer (int) números inteiros
-   * Double (double) Números 0.0000
-   * Float (float) Números 0.000
-   * char (A C)
-   * Date (data)
-   * void
-   */
-  /*
-   * Body
-   */
+
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
+  public UserModel create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      System.out.println("Usuário já existe");
+      return null;
+    } 
+    
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 }
